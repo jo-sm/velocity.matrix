552be540ddbe0c1b74e0b446a05220ac2d227a28
Joshua Smock <joshua.smock@gmail.com>
2014-06-22 15:17:32 -0400

    Rename class to cssMatrix2D for possible future work with 3D matrices; Create Matrix variable for matrix operations; CSS angle parser; Optimize some operations to use addition rather than multiplication

--- cssMatrix.js
+++ cssMatrix.js
@@ -1,45 +1,112 @@
-cssMatrix = function(obj) {
-    matrix = null;
-    
-    var radians = function(deg) {
-        return deg * (Math.PI/180);
-    }
-
-	var multiply = function(_m) {
-        if(matrix == null) {
-            matrix = _m;
-            return;
-        }
-		// Go through each row against each current matrix's column
-		_n = [
-			[],
-			[],
-			[]
-		];
-		matrix.forEach(function(ele, i) {
-			_n[i][0] = ele[0] * _m[0][0] + ele[1] * _m[1][0] + ele[2] * _m[2][0];
-			_n[i][1] = ele[0] * _m[0][1] + ele[1] * _m[1][1] + ele[2] * _m[2][1];
-			_n[i][2] = ele[0] * _m[0][2] + ele[1] * _m[1][2] + ele[2] * _m[2][2];
+var Matrix = {
+	add: function(_m1, _m2) {
+		// Add each row to the other row
+		_n = [];
+		// _m1 num rows must equal _m2 num rows
+		if(_m1.length !== _m2.length) {
+			return false;
+		}
+		
+		_m1.forEach(function(ele, i) {
+			// Each row needs to be the same size
+			// TODO: Case when row is length 1
+			if(!(ele.length == _m2[i].length)) {
+				if(ele.length > _m2[i].length) {
+					_i = ele.length - _m2[i].length;
+					for(var j = 0; j < _i; j++) {
+						_m2[j].push(0);
+					}
+				} else {
+					_i = _m2[i].length - ele.length;
+					for(var j = 0; j < _i; j++) {
+						ele.push(0);
+					}
+				}
+			}
+			
+			// Add each row
+			_k = [];
+			for(var j = 0; j < ele.length; j++) {
+				_k.push(ele[j] + _m2[i][j]);
+			}
+			_n.push(_k);
 		});
-		matrix = _n;
-	};
+		return _n;
+	},
 
+	multiply: function(_m1, _m2) {
+		// _m1 is generally var matrix
+		// _m2 is the matrix multiplying against matrix
+		// _m1 row size must equal m2 column size (num elements)
+		_n = [];
+		
+		if(_m1[0].length !== _m2.length) {
+			return false;
+		}
+		
+		_m1.forEach(function(ele, i) {
+			_k = [];
+			ele.forEach(function(_e, j) {
+                sum = 0;
+				for(var k = 0; k < ele.length; k++) {
+					sum += ele[k] * _m2[k][j];
+				}
+				_k.push(sum);
+			});
+			_n.push(_k);
+		});
+		return _n;
+	}
+};
+
+var cssMatrix2D = function(obj) {
+	// CSS 2d matrix transformations
+	var _matrix = [
+		[1,0,0],
+		[0,1,0],
+		[0,0,1]
+	];
+    
+    // Translates a CSS angle into radians
+    // Returns 0 if unable to parse (is this desired, or should it return an error?)
+    var translateAngle = function(angle) {
+        if(typeof angle == "number") {
+            // Assume that the value is a degree
+             return angle * (Math.PI/180);
+        } else if(typeof angle == "string") {
+            _a = angle.match(/^[0-9]*/);
+            if(angle.match(/^[0-9]*deg$/)) {
+                return _a * (Math.PI/180);
+            } else if(angle.match(/^[0-9]*grad$/)) {
+                return _a * (Math.PI/200);
+            } else if(angle.match(/^[0-9]*rad$/)) {
+                return _a;
+            } else if(angle.match(/^[0-9]*turn$/)) {
+                return _a * 2 * Math.PI;
+            } else {
+                return 0;   
+            }
+        } else {
+            return 0;
+        }
+    };
+	
 	var translateX = function(val) {
 		var _m = [
-			[1, 0, val],
-			[0, 1, 0],
-			[0, 0, 1]
+			[0,0,val],
+			[0,0,0],
+			[0,0,1]
 		];
-		multiply(_m);
+		_matrix = Matrix.add(_matrix, _m);
 	};
 
 	var translateY = function(val) {
 		var _m = [
-			[1, 0, 0],
-			[0, 1, val],
-			[0, 0, 1]
+			[0,0,0],
+			[0,0,val],
+			[0,0,1]
 		];
-		multiply(_m);
+		_matrix = Matrix.add(_matrix, _m);
 	};
 
 	var scale = function(x, y) {
@@ -48,16 +115,16 @@ cssMatrix = function(obj) {
 			[0, y, 0],
 			[0, 0, 1]
 		];
-		multiply(_m);
+		_matrix = Matrix.multiply(_matrix, _m);
 	};
 
 	var scaleX = function(val) {
 		var _m = [
-			[val, 0, 0],
-			[0, 1, 0],
-			[0, 0, 1]
+			[val],
+			[0],
+			[0]
 		];
-		multiply(_m);
+		_matrix = Matrix.multiply(_matrix, _m);
 	};
 
 	var scaleY = function(val) {
@@ -66,45 +133,45 @@ cssMatrix = function(obj) {
 			[0, val, 0],
 			[0, 0, 1]
 		];
-		multiply(_m);
+		_matrix = Matrix.multiply(_matrix, _m);
 	};
 
 	var skew = function(x, y) {
 		var _m = [
-			[1, Math.tan(radians(x)), 0],
-			[Math.tan(radians(y)), 1, 0],
+			[1, Math.tan(translateAngle(x)), 0],
+			[Math.tan(translateAngle(y)), 1, 0],
 			[0, 0, 1]
 		];
-		multiply(_m);
+		_matrix = Matrix.multiply(_matrix, _m);
 	};
 
 	var skewX = function(val) {
 		var _m = [
-			[1, Math.tan(radians(val)), 0],
+			[1, Math.tan(translateAngle(val)), 0],
 			[0, 1, 0],
 			[0, 0, 1]
 		];
-		multiply(_m);
+		_matrix = Matrix.multiply(_matrix, _m);
 	};
 
 	var skewY = function(val) {
 		var _m = [
 			[1, 0, 0],
-			[Math.tan(radians(val)), 1, 0],
+			[Math.tan(translateAngle(val)), 1, 0],
 			[0, 0, 1]
 		];
-		multiply(_m);
+		_matrix = Matrix.multiply(_matrix, _m);
 	};
 
 	var rotate = function(val) {
 		var _m = [
-			[Math.cos(radians(val)), -Math.sin(radians(val)), 0],
-			[Math.sin(radians(val)), Math.cos(radians(val)), 0],
+			[Math.cos(translateAngle(val)), -Math.sin(translateAngle(val)), 0],
+			[Math.sin(translateAngle(val)), Math.cos(translateAngle(val)), 0],
 			[0, 0, 1]
 		];
-		multiply(_m);
+		_matrix = Matrix.multiply(_matrix, _m);
 	};
-
+    
 	for (var i in obj) {
 		switch (i) {
 			case 'translateX':
@@ -136,5 +203,6 @@ cssMatrix = function(obj) {
 				break;
 		}
 	}
-	return [self.matrix[0][0], self.matrix[1][0], self.matrix[0][1], self.matrix[1][1], self.matrix[0][2], self.matrix[1][2]];
-}
+    
+	return [_matrix[0][0], _matrix[1][0], _matrix[0][1], _matrix[1][1], _matrix[0][2], _matrix[1][2]];
+};
